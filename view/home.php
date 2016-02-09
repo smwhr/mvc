b1c069631014a9f86a9c6d6837961d81a91f8d2a
Ju/TroisYaourts <julien@troisyaourts.com>
2016-02-09 12:11:59 +0100

    delete ajax

--- view/home.php
+++ view/home.php
@@ -3,15 +3,29 @@
 <head>
   <meta charset="UTF-8">
   <title>Destinations</title>
+  <script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
 </head>
 <body>
   <ul>
   <?php foreach($destinations as $ville):?>
-    <li>
+    <li class="city" id="city-<?php echo $ville['id']?>">
     <?php echo $ville["name"];?>
-    <a href="/city/delete/<?php echo $ville['id']?>">X</a>
+    <a class="city-delete" href="#" data-cityid="<?php echo $ville['id']?>">X</a>
     </li>
   <?php endforeach;?>
   </ul>
+
+<script>
+$(document).on('click','.city-delete', function(e){
+
+  var city_id = $(this).data('cityid');
+  $.post('/city/delete/'+city_id,function(data){
+    console.log(data)
+  })
+
+  e.preventDefault();
+})
+</script>
+
 </body>
 </html>
